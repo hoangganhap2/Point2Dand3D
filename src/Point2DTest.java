84e5846510bd6990efb2761f0e33f5bd40e8ef66
Hoang <cct.cry@gmai.com>
2019-11-02 22:50:13 +0700

    p

---
+++ src/Point2DTest.java
@@ -0,0 +1,9 @@
+public class Point2DTest {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D();
+        System.out.println(point2D);
+
+        point2D = new Point2D(2.6f,3.5f);
+        System.out.println(point2D);
+    }
+}
---
+++ src/Point3DTest.java
@@ -0,0 +1,9 @@
+public class Point3DTest {
+    public static void main(String[] args) {
+        Point3D point3D = new Point3D();
+        System.out.println(point3D);
+
+        point3D = new Point3D(3.5f,2.5f,3.6f);
+        System.out.println(point3D);
+    }
+}
